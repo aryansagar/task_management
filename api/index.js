11d8896309b3b82edd6b2f9de2c51cd907c1f4a9
Aryan <aryan@Aryans-MacBook-Air.local>
2024-01-05 19:44:08 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('Connected to db');
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
-
 app.listen(3000,()=>{
-    console.log("server is running on port 3000!!!")
+    console.log("server is running on port 3000")
 })
